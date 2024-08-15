83ceabafc07ce73e0f37e9797e3e2a01a83f1413
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2024-08-14 19:15:30 -0500

    Adds phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   passwordless_with :email # <-- here! this needs to be a column in `users` table
 
+
+  passwordless_with :phone
+  #
+  # def self.fetch_resource_for_passwordless(email)
+  #   find_or_create_by(email: email)
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_173052) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_15_001026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_173052) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
